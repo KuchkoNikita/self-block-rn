3c19ddd1861152feac1c271e271a63aa9d344181
kawasaki646 <kawasaki646@gmail.com>
2020-10-27 13:52:49 +0300

    implemented bottom Nabigation and BookedScreen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
-import { Post } from '../components/Post';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
+import { Post } from '../components/Post';
 import { DATA } from '../data'
 
 export const MainScreen = ({ navigation }) => {
 
   const openPostHandler = post => {
-    navigation.navigate(
-      'Post', 
-      { 
-        postId: post.id,
-        date: post.date
-      }
+    navigation.navigate('Post', { 
+      postId: post.id, 
+      date: post.date,
+      booked: post.booked
+    }
     )
   }
 
@@ -21,19 +22,32 @@ export const MainScreen = ({ navigation }) => {
       <FlatList 
         data={DATA}
         keyExtractor={post => post.id.toString()}
-        renderItem={({ item }) => (
-          <Post 
-            post={ item }
-            onOpen={ openPostHandler }
-          />
-        )}
+        renderItem={({ item }) => <Post post={ item } onOpen={ openPostHandler } />}
       />
     </View>
   );
 }
 
 MainScreen.navigationOptions = {
-  headerTitle: 'Мой блог'
+  headerTitle: 'Мой блог',
+  headerRight: () =>  (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Take photo'
+        iconName='ios-camera'
+        onPress={() => console.log('Press photo')}
+      />
+    </HeaderButtons>
+  ),
+  headerLeft: () =>  (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Toggle Drawer'
+        iconName='ios-menu'
+        onPress={() => console.log('Press photo')}
+      />
+    </HeaderButtons>
+  )
 }
 
 const styles = StyleSheet.create({
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,28 +1,73 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Image, Button, ScrollView, Alert } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
+import { THEME } from '../theme'
+import { DATA } from '../data';
 
 export const PostScreen = ({ navigation }) => {
   const postId = navigation.getParam('postId');
+  const post = DATA.find(p => p.id === postId)
+
+  const removeHandler = () => {
+    Alert.alert(
+      'Удаление поста',
+      'Вы точно хотите удалить пост ?',
+      [
+        {
+          text: 'Отменить',
+          style: 'cancel'
+        },
+        { text: 'Удалить', style: 'destructive', onPress: () => {} }
+      ],
+      { cancelable: false }
+    );
+  }
 
   return (
-    <View style={styles.center}>
-      <Text>{ postId }</Text>
-    </View>
+    <ScrollView style={styles.center}>
+      <Image source={{ uri: post.img }} style={ styles.image } />
+      <View style={ styles.textWrap }>
+        <Text style={ styles.title }>{ post.text }</Text>
+      </View>
+      <Button 
+        title='Удалить' 
+        color={ THEME.DANGER_COLOR } 
+        onPress={removeHandler}
+      /> 
+    </ScrollView>
   )
 }
 
 PostScreen.navigationOptions = ({ navigation }) => {
   const date = navigation.getParam('date');
+  const booked = navigation.getParam('booked');
+  const iconName = booked ? 'ios-star' : 'ios-star-outline';
 
   return { 
-    headerTitle: 'Пост от' + date.toLocaleDateString()
+    headerTitle: 'Пост от ' + new Date(date).toLocaleDateString(),
+    headerRight: () =>  (
+      <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item
+          title='Take photo'
+          iconName={iconName}
+          onPress={() => console.log('Press photo')}
+        />
+      </HeaderButtons>
+    ),
   }
 }
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  image: {
+    width: '100%',
+    height: 200
+  },
+  textWrap: {
+    padding: 10
+  },
+  title: {
+    fontFamily: 'open-regular'
+  },
 })
